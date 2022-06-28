07930dd8cfb75988c3c6622ea32149eb8d108f83
Johannes Bauer <joe@johannes-bauer.com>
2022-06-28 15:28:23 +0200

    Rename project
    Renamed everything to airc so that we have at least this groundwork
    settled for the future.

--- airc/ReplyCode.py
+++ airc/ReplyCode.py
@@ -1,20 +1,20 @@
-#	pyirclib - Python IRC client library with DCC and anonymization support
+#	airc - Python asynchronous IRC client library with DCC support
 #	Copyright (C) 2016-2022 Johannes Bauer
 #
-#	This file is part of pyirclib.
+#	This file is part of airc.
 #
-#	pyirclib is free software; you can redistribute it and/or modify
+#	airc is free software; you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation; this program is ONLY licensed under
 #	version 3 of the License, later versions are explicitly excluded.
 #
-#	pyirclib is distributed in the hope that it will be useful,
+#	airc is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
 #
 #	You should have received a copy of the GNU General Public License
-#	along with pyirclib; if not, write to the Free Software
+#	along with airc; if not, write to the Free Software
 #	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #	Johannes Bauer <JohannesBauer@gmx.de>
